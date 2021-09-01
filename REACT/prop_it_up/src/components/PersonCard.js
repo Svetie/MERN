6b4e52d6f4909baf2e11bf524041b17236943588
Svetie <lighta562@gmail.com>
2021-09-01 19:06:13 -0400

    put it together

--- REACT/prop_it_up/src/components/PersonCard.js
+++ REACT/prop_it_up/src/components/PersonCard.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
 
 class PersonCard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            age: this.props.age  //this can change
+        };
+    }
+
     render(){
         const { firstName, lastName, age, hairColor } = this.props;
+
+        const happyBday = () =>{
+            // console.log(`Age before is ${this.state.age}`);
+            this.setState({age: this.state.age+1})
+            // console.log(`Age after is ${this.state.age}`);
+        }
+
         return(
             <div>
                 <h1>{ lastName }, { firstName}  </h1>
-                <p>Age: {age} </p>
+                <p>Age: {this.state.age} </p>
                 <p>Hair Color: { hairColor } </p>
+                <p><button onClick={happyBday}>Birthday Button for {firstName} {lastName}</button></p>
             </div>
         );
     }
