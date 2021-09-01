82eb9e6bf35947b7601ca949a154214fb68c1968
Svetie <lighta562@gmail.com>
2021-09-01 16:31:15 -0400

    state added

--- REACT/ClassComponents/component_demo_menu/src/App.js
+++ REACT/ClassComponents/component_demo_menu/src/App.js
@@ -6,14 +6,14 @@ function App() {
   return (
     <div className="App">
       <h1>Dojo Diner</h1>
-      <Menu dishName="Momo" price={12.99}>
+      <Menu dishName="Momo" price={12.99} numMeals={10}>
         <p>Nepali dumpings</p>
         <p>Vegan gluten-free options available</p>
       </Menu>
-      <Menu dishName="Maduros" price={12.99}>
+      <Menu dishName="Maduros" price={12.99} numMeals={5}>
         <p>Delicious sweet plaintain</p>
       </Menu>
-      <Menu dishName="Buffulo Chicken Pizza" price={12.99}>
+      <Menu dishName="Buffulo Chicken Pizza" price={12.99} numMeals={12}>
         <p>Straight out of NY State</p>
         <p>Contains wheat</p>
       </Menu>
--- REACT/ClassComponents/component_demo_menu/src/components/Menu.jsx
+++ REACT/ClassComponents/component_demo_menu/src/components/Menu.jsx
@@ -1,14 +1,33 @@
 import React, { Component } from "react";
 
 class Menu extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            numMeals: this.props.numMeals   //this can change
+        };
+    }
+
     render(){
+
+        // component has a variable unique to the
+        // instance of that component, then we will use
+        // a state variable
+        const doSomething = () =>{
+            console.log(`You clicked ${this.props.dishName}`);
+            // this.state.numMeals++;
+            //{ this.state.position = "Off" } }>Flip Switch
+            this.setState({numMeals: this.state.numMeals+1})
+        }
          // <></> // empty expression
         return (
             <>
             <div className="menuItem">
                 <h3>Dish name: {this.props.dishName}</h3>
                 <h5>Price: {this.props.price}</h5>
-                <p>Description: {this.props.children}</p>
+                <div>Description: {this.props.children}</div>
+                <p>Number of Meals: {this.state.numMeals}</p>
+                <p><button onClick={doSomething}>Serve Meal</button></p>
             </div>
             </>
         )
