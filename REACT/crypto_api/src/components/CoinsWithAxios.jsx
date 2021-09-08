e06633c085d49988e6fa244edcacde94e43d79f8
Svetie <lighta562@gmail.com>
2021-09-08 12:05:58 -0400

    useEffect added

--- REACT/crypto_api/src/components/CoinsWithAxios.jsx
+++ REACT/crypto_api/src/components/CoinsWithAxios.jsx
@@ -1,13 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const CoinsWithAxios = () => {
 
     const [allCoins, setAllCoins] = useState([]);
-    // const [allPokemon, setAllPokemons] = useState([]);
 
-    const clickHandler = ()=>{
-        console.log("clicked!")
+    // useEffect is a hook function that accepts a function to run as soon as the page loads up/renders
+    useEffect(() => {
+        console.log('page loaded');
         axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false")
         //.then() means when the responseponse is recieved back successfully from the api call, what to do with it
         .then(response=>{
@@ -20,9 +20,9 @@ const CoinsWithAxios = () => {
             // })
 
             // sort in descending order
-            response.data.sort(function (a,b) {
-                return b.current_price - a.current_price;
-            })
+            // response.data.sort(function (a,b) {
+            //     return b.current_price - a.current_price;
+            // })
 
             setAllCoins(response.data);
             // pokemon response.results
@@ -31,12 +31,26 @@ const CoinsWithAxios = () => {
         .catch(err=>{
             console.log(err)
         }) //.catch() means if there were any errors that came up in our api request, this is where we will handle those errors
+    },[]) 
+    // useEffect allows us to modify a state variable upon loading of the page
+
+
+    const clickHandler = ()=>{
+        console.log("clicked!")
+        //sort in descending order
+        let coins = [...allCoins];
+
+        coins.sort(function (a,b) {
+            return b.current_price - a.current_price;
+        })
+
+        setAllCoins(coins);
     }
 
 
     return (
         <div>
-            <button onClick={clickHandler} className="btn btn-success">Show me the money!</button>
+            <button onClick={clickHandler} className="btn btn-success">Sort in Descending Order</button>
         {
         allCoins.map((coin, i)=>{
             return <div key={i} className="card" style={{backgroundImage: `url(${coin.image})`}}>
